4ab4dc39c69624445e62e0aa20648af076146320
Wagner Moreira <wagnercomputacao.unitri@yahoo.com.br>
2016-12-28 11:38:41 -0200

    trabalhando com datas - inserindo data atual

--- Capítulo 03 - Trabalhando com datas.sql
+++ Capítulo 03 - Trabalhando com datas.sql
@@ -14,4 +14,14 @@ select * from professor
 	where data_nascimento >= '1982-07-21 00:00:00'
     and data_nascimento <= '1992-08-09 23:59:59'
     and horario_entrada = '07:30:00';
-		
+
+-- Insert com data atual
+
+select * from matricula;
+
+insert into matricula (
+	aluno_id
+    , curso_id
+    , data_matricula
+
+) values (3, 3, now());
